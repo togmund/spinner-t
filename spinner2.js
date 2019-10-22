b961a8058ec79955f72a3c728122644d9470a392
Ted Ogmundson <togmund@gmail.com>
2019-10-22 23:22:03 +0000

    updated spinner to include a second funnier spinner

--- spinner2.js
+++ spinner2.js
@@ -12,10 +12,24 @@ const spinner2 = () => {
   }
 }
 
+const spinnerPs = () => {
+  const sprites =
+  [ 'p'
+  , 'q'
+  , 'd'
+  , 'b'
+  , 'p'
+  ]
+  for (let rad = 0; rad < sprites.length; rad++) {
+    setTimeout(() => { process.stdout.write(`\r${sprites[rad]}   `); }, rad * 200);
+  }
+}
+
+
 // Define take a spinner that rotates a single time and rotate it as many times as you'd like.
 const spinnerCaller = (spinnerFunc, cycles) => {
   for (let cyc = 0; cyc < cycles; cyc++) { setTimeout(() => { spinnerFunc(); }, cyc * 1000); }
 }
 
 // Run the function to spin the spinner
-spinnerCaller(spinner2, 5);
+spinnerCaller(spinnerPs, 5);
